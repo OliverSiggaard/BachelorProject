c9dc6594e1637d044d887de9279a57f0a43313a2
Oliver Siggaard <s204450@student.dtu.dk>
2024-03-15 12:27:39 +0100

    Interface for PathFinding algorithms and Enum for droplet moves

---
+++ src/main/java/com/digitalmicrofluidicbiochips/bachelorProject/executor/path_finding/DropletMove.java
@@ -0,0 +1,10 @@
+package com.digitalmicrofluidicbiochips.bachelorProject.executor.path_finding;
+
+public enum DropletMove {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    WAIT,
+    BLOCKED
+}
---
+++ src/main/java/com/digitalmicrofluidicbiochips/bachelorProject/executor/path_finding/IPathFinder.java
@@ -0,0 +1,8 @@
+package com.digitalmicrofluidicbiochips.bachelorProject.executor.path_finding;
+
+import com.digitalmicrofluidicbiochips.bachelorProject.model.dmf_platform.Droplet;
+import com.digitalmicrofluidicbiochips.bachelorProject.model.dmf_platform.Electrode;
+
+public interface IPathFinder {
+    DropletMove getMove(Droplet activeDroplet, Electrode[][] availableGrid, int targetX, int targetY);
+}
